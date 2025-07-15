c5b6db7f51ae8bb9f3aed9843191c0acf59525a4
cybermago <cybermago@outlook.com>
2025-07-15 00:25:45 -0300

    update vercel1.0

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 # exit on error
 set -o errexit
 
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "Coletando arquivos estáticos..."
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 
 echo "Iniciando migraçoes"
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
