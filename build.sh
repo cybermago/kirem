953a670b7371b1acf173db84ac18eb0e4f0d7b5d
cybermago <cybermago@outlook.com>
2025-07-15 00:20:48 -0300

    update vercel

--- build.sh
+++ build.sh
@@ -6,5 +6,9 @@ python -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
+echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --no-input
+
+echo "Iniciando migraçoes"
+python manage.py makemigrations
 python manage.py migrate
